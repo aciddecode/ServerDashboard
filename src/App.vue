46d020eef334cb9e427fa78968a88cabdb9a202d
aciddecode <tareq@djamous.de>
2018-11-01 10:55:29 +0100

    Initial

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Dashboard/>
+  </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import Dashboard from './components/Dashboard.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Dashboard
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,9 @@
+<template>
+    <b-container>
+        <b-row>
+            <b-col>
+                <h1>Server Admin</h1>
+            </b-col>
+        </b-row>
+    </b-container>    
+</template>
